31cfeffbec3a282eca6879a02238674ba92aad88
MahmoudDawood <30799773+MahmoudDawood@users.noreply.github.com>
2021-09-24 11:56:44 +0200

    Delete advanced directory

--- advanced/conditinal-operator.js
+++
@@ -1,8 +0,0 @@
-const team = ['Harry', 'Ron']
-
-const less = () => console.log('Team size: 3')
-const more = () => console.log('Too many people on your team')
-
-team.length <= 4 ? less() : more()
-
-console.log(team.length <= 4 ? `Team size: ${team.length}` : 'Too many players')
