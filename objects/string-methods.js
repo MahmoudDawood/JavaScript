c769f4417e4db46f449c71730e2ef03c71a9defa
MahmoudDawood <30799773+MahmoudDawood@users.noreply.github.com>
2021-09-24 11:57:39 +0200

    Delete objects directory

--- objects/string-methods.js
+++
@@ -1,7 +0,0 @@
-let isValidPasssword = function(password){
-    return password.length > 8 && !password.includes('password')
-}
-
-console.log(isValidPasssword('afsdf'))
-console.log(isValidPasssword('faksjhdfpiasdfba9'))
-console.log(isValidPasssword('dfssdfsdfpassword'))
