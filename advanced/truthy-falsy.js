4147085a7d86e5c9cda000bac99710c9d78ed3ab
MahmoudDawood <30799773+MahmoudDawood@users.noreply.github.com>
2021-09-26 15:45:14 +0200

    Delete advanced directory

--- advanced/truthy-falsy.js
+++
@@ -1,4 +0,0 @@
-const products = []
-const product = products[0]
-
-console.log(product ? 'Found!' : 'Not found')
--- advanced/type-coersion.js
+++
@@ -1,5 +0,0 @@
-const value = false + 12
-const type = typeof value
-
-console.log(value)
-console.log(type)
